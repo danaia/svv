37fded40b2f6fdc073dcda7495af7ba6102494c3
danaia <martinelli@gmail.com>
2022-02-21 00:09:36 -0500

    logo

--- src/routes/blogpost/__layout.reset.svelte
+++ src/routes/blogpost/__layout.reset.svelte
@@ -5,7 +5,7 @@
 <main class="bg-black">
   <nav>
     <div class="grid grid-cols-2 md:w-4/5 mx-auto p-5 bg-blue-100 ">
-      <div class="text-xl font-bold uppercase text-gray-700">Nestr</div>
+      <div class="text-xl font-bold uppercase text-gray-700">Svv</div>
       <div class="flex justify-end links items-center">
         <span><a href="/">Home</a></span>
         <span><a href="/blog">Blog</a></span>
